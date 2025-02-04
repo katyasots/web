24e6bbc6d9cf78b12b84f3c9e42e91ff7807bbe1
katyasots <katesots.404k@gmail.com>
2025-02-04 18:55:30 +0300

    ok

--- tetris/ind.js
+++
@@ -1,8 +0,0 @@
-// index.js
-
-import './game.js';
-import './utils.js';
-import './records.js';
-
-// старт игры
-rAF = requestAnimationFrame(loop);
--- tetris/utils.js
+++
@@ -1,7 +0,0 @@
-// Функция возвращает случайное число в заданном диапазоне
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
